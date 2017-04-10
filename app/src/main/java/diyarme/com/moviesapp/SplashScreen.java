3a2e74d3bfee9ce37ff053ee3ee03349a0913fbe
alahammad <ip7@live.de>
2017-04-10 10:45:14 +0300

    add comments

--- app/src/main/java/diyarme/com/moviesapp/SplashScreen.java
+++ app/src/main/java/diyarme/com/moviesapp/SplashScreen.java
@@ -14,6 +14,7 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
+        // add comments
         getSupportActionBar().hide();
         controlSplashScreen();
     }
